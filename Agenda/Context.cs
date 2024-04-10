3da265abc45063653a9495b711cd1ba0550e8eb4
diegolr-avanade <diego.lopez.reduello@avanade.com>
2024-04-10 13:12:17 +0200

    ✨Conexión a la base de datos realizada

--- Agenda/Context.cs
+++ Agenda/Context.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,39 @@ namespace Agenda
 {
     internal class Context
     {
+        private static string connectionString = @"Data Source=WINAP3MGRRULDGQ\SQLEXPRESS;Initial Catalog=Agenda;Application Name=Agenda;Integrated Security=SSPI";
+
+        private static SqlConnection connection = new SqlConnection(connectionString);
+
+        public static void OpenSQLConnetion()
+        {
+            try
+            {
+                connection.ConnectionString = connectionString;
+
+                connection.Open();
+
+                Debug.WriteLine("\n¡Base de datos conectada!\n");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("\nError al conectar a la base de datos... " + ex.Message + "\n");
+            }
+        }
+
+        public static void CloseSQLConnection()
+        {
+            try
+            {
+                connection.Close();
+                connection.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("\n¡Error al cerrar la conexión con base de datos!\n" + ex.Message);
+            }
+        }
 
     }
 }
+
--- Agenda/Program.cs
+++ Agenda/Program.cs
@@ -11,6 +11,7 @@ namespace Agenda
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            Context.OpenSQLConnetion();
             Application.Run(new Agenda());
         }
     }

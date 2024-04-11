941869ab310af0345e2c2111443f5d7016b1724f
diegolr-avanade <diego.lopez.reduello@avanade.com>
2024-04-11 09:45:14 +0200

    🔧 Mejorar código de conexión a BBDD

--- Agenda/Context.cs
+++ Agenda/Context.cs
@@ -6,43 +6,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
 namespace Agenda
 {
-    internal class Context
+    internal static class Context
     {
-        private static string connectionString = @"Data Source=WINAP3MGRRULDGQ\SQLEXPRESS;Initial Catalog=Agenda;Application Name=Agenda;Integrated Security=SSPI";
+        private static readonly string connectionString = @"Data Source=WINAP3MGRRULDGQ\SQLEXPRESS;Initial Catalog=Agenda;Application Name=Agenda;Integrated Security=SSPI";
 
-        private static SqlConnection connection = new SqlConnection(connectionString);
-
-        public static void OpenSQLConnetion()
+        public static SqlConnection OpenSQLConnetion()
         {
+            SqlConnection connection = new SqlConnection(connectionString);
             try
             {
-                connection.ConnectionString = connectionString;
-
                 connection.Open();
-
                 Debug.WriteLine("\n¡Base de datos conectada!\n");
             }
             catch (Exception ex)
             {
-                Debug.WriteLine("\nError al conectar a la base de datos... " + ex.Message + "\n");
+                Debug.WriteLine($"\nError al conectar a la base de datos... {ex.Message}\n");
             }
+            return connection;
         }
 
-        public static void CloseSQLConnection()
+        public static void CloseSQLConnection(SqlConnection connection)
         {
             try
             {
                 connection.Close();
                 connection.Dispose();
+                Debug.WriteLine("\n¡Conexión con base de datos cerrada!\n");
             }
             catch (Exception ex)
             {
-                Debug.WriteLine("\n¡Error al cerrar la conexión con base de datos!\n" + ex.Message);
+                Debug.WriteLine($"\n¡Error al cerrar la conexión con base de datos!\n{ex.Message}");
             }
         }
-
     }
 }
+
 
